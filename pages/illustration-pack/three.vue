d045487fc1cd7a67c33fdcd39224eead05418be5
Sahil Ranpuri <sahilranpuri@gmail.com>
2020-04-02 11:04:45 +0530

    Fix: thrid mockup font-family changed

--- pages/illustration-pack/three.vue
+++ pages/illustration-pack/three.vue
@@ -57,12 +57,12 @@ export default {
       {
         rel: 'stylesheet',
         href:
-          'https://fonts.googleapis.com/css?family=EB+Garamond:400,700&display=swap'
+          'https://fonts.googleapis.com/css?family=Nanum+Gothic:400,700&display=swap'
       },
-      {
+       {
         rel: 'stylesheet',
         href:
-          'https://fonts.googleapis.com/css?family=Oswald:300,400&display=swap'
+          'https://fonts.googleapis.com/css?family=Lato&display=swap'
       },
     ]
   })
@@ -74,7 +74,7 @@ export default {
 body {
   margin: 0;
   padding: 0;
-  font-family: 'EB Garamond';
+   font-family: 'Nanum Gothic';
 }
 .container {
   width: 720px;
@@ -116,7 +116,7 @@ body {
   width: 215px;
   font-weight: 300;
   font-size: 21px;
-  font-family: 'Oswald';
+  font-family: 'Lato';
 }
 
 .heroImg {
