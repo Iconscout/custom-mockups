38bf7a9437c05e9ff72df032b2ea14b96446a412
Sahil Ranpuri <sahilranpuri@gmail.com>
2020-12-08 13:30:58 +0530

    Feature: Icon mockup ui

---
+++ pages/icon-pack/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container">
+    <img
+      src="https://s3.wasabisys.com/icons-dev/icon/premium/svg/9.svg?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=SRAPV43ZKR3GZV5VEPBC%2F20201208%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20201208T061539Z&X-Amz-SignedHeaders=host&X-Amz-Expires=259200&X-Amz-Signature=405fd1805dcbbfcaca12e86b946d37e9ef51b1f6b9ec113b428a6e9846fb937f"
+      class="image"
+    />
+    <img
+      src="https://s3.wasabisys.com/icons-dev/icon/premium/svg/9.svg?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=SRAPV43ZKR3GZV5VEPBC%2F20201208%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20201208T061539Z&X-Amz-SignedHeaders=host&X-Amz-Expires=259200&X-Amz-Signature=405fd1805dcbbfcaca12e86b946d37e9ef51b1f6b9ec113b428a6e9846fb937f"
+      class="image"
+    />
+    <img
+      src="https://s3.wasabisys.com/icons-dev/icon/premium/svg/9.svg?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=SRAPV43ZKR3GZV5VEPBC%2F20201208%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20201208T061539Z&X-Amz-SignedHeaders=host&X-Amz-Expires=259200&X-Amz-Signature=405fd1805dcbbfcaca12e86b946d37e9ef51b1f6b9ec113b428a6e9846fb937f"
+      class="image"
+    />
+    <img
+      src="https://s3.wasabisys.com/icons-dev/icon/premium/svg/9.svg?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=SRAPV43ZKR3GZV5VEPBC%2F20201208%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20201208T061539Z&X-Amz-SignedHeaders=host&X-Amz-Expires=259200&X-Amz-Signature=405fd1805dcbbfcaca12e86b946d37e9ef51b1f6b9ec113b428a6e9846fb937f"
+      class="image"
+    />
+    <img
+      src="https://s3.wasabisys.com/icons-dev/icon/premium/svg/9.svg?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=SRAPV43ZKR3GZV5VEPBC%2F20201208%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20201208T061539Z&X-Amz-SignedHeaders=host&X-Amz-Expires=259200&X-Amz-Signature=405fd1805dcbbfcaca12e86b946d37e9ef51b1f6b9ec113b428a6e9846fb937f"
+      class="image"
+    />
+    <img
+      src="https://s3.wasabisys.com/icons-dev/icon/premium/svg/9.svg?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=SRAPV43ZKR3GZV5VEPBC%2F20201208%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20201208T061539Z&X-Amz-SignedHeaders=host&X-Amz-Expires=259200&X-Amz-Signature=405fd1805dcbbfcaca12e86b946d37e9ef51b1f6b9ec113b428a6e9846fb937f"
+      class="image"
+    />
+  </div>
+</template>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+.container {
+  width: 360px;
+  height: 240px;
+  background: #ffffff;
+  display: inline-flex;
+  flex-wrap: wrap;
+  padding: 12px 15px;
+  overflow: hidden;
+}
+
+.image {
+  width: 80px;
+  height: 80px;
+  max-width: 80px;
+  max-height: 80px;
+  margin: 14px 15px;
+}
+</style>
