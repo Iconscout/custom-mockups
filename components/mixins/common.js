41b0d9ef40c628a1af644f654fbe915a761aa9a6
Zaid Kureshi <imzedi@yahoo.com>
2021-01-08 14:59:49 +0530

    Fix: typo

--- components/mixins/common.js
+++ components/mixins/common.js
@@ -5,7 +5,7 @@ export default {
     return {
       assets: [
         { value: 'illustration', text: 'Illustrations' },
-        { value: '3d', text: '3D illutrations' },
+        { value: '3d', text: '3D illustration' },
         { value: 'icon', text: 'Icons' }
       ]
     }
