6ee2fb2bdc23480d26f4922fd76c1c9817bd0693
Zaid Kureshi <imzedi@yahoo.com>
2021-03-30 20:14:13 +0530

    Fix: watermark block added

--- pages/video-preview.vue
+++ pages/video-preview.vue
@@ -1,11 +1,8 @@
 <template>
-  <div class="wrapper">
-    <img :src="params.url" width="378" height="378" />
-    <div class="box">
-      <div class="watremark">
-        <img src="@/assets/watermark/logo.svg" width="150" />
-        <div class="username">@{{ params.username }}</div>
-      </div>
+  <div class="box">
+    <div class="watremark">
+      <img src="@/assets/watermark/logo.svg" width="300" />
+      <div class="username">@{{ params.username }}</div>
     </div>
   </div>
 </template>
@@ -21,24 +18,9 @@ export default {
 </script>
 
 <style>
-.wrapper {
-  position: relative;
-  padding: 61px;
-  width: 500px;
-  height: 500px;
-  background: #ffffff;
-}
-
 .box {
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  margin: auto;
-  width: fit-content;
-  height: fit-content;
-  z-index: 10;
+  width: 364px;
+  height: 129px;
 }
 
 .watremark {
@@ -47,15 +29,17 @@ export default {
   justify-content: center;
   align-items: center;
   background: rgba(71, 71, 71, 0.06);
-  padding: 16px;
+  padding: 32px;
 }
 
 .username {
+  font-family: 'Averta';
   position: absolute;
-  bottom: 16px;
-  left: 59px;
+  bottom: 30px;
+  left: 117px;
   color: #474747;
   opacity: 0.4;
-  font-size: 8px;
+  font-size: 22px;
+  line-height: 28px;
 }
 </style>
