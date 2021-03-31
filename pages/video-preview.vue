04f1045446f6e7caf720c78b46776f6f75d9a93f
Zaid Kureshi <48899828+imzedi@users.noreply.github.com>
2021-03-31 17:14:01 +0530

    feat: Video watermark mockup (#9)
    * Try: Video watermark preview
    
    * Fix: watermark block added
    
    * Fix: typo

---
+++ pages/video-preview.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="box">
+    <div class="watermark">
+      <img src="@/assets/watermark/logo.svg" width="300" />
+      <div class="username">@{{ params.username }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    params() {
+      return this.$route.query
+    }
+  }
+}
+</script>
+
+<style>
+.box {
+  width: 364px;
+  height: 129px;
+}
+
+.watermark {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: rgba(71, 71, 71, 0.06);
+  padding: 32px;
+}
+
+.username {
+  font-family: 'Averta';
+  position: absolute;
+  bottom: 30px;
+  left: 117px;
+  color: #474747;
+  opacity: 0.4;
+  font-size: 22px;
+  line-height: 28px;
+}
+</style>
